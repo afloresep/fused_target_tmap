14af1cd60f7e28477e2c72c9ff6cb36f427ed772
Alejandro Flores <afloresep01@gmail.com>
2024-09-10 10:52:47 +0200

    Update mhfp_tmap.py
    Simplified version, now only one function

--- mhfp_tmap.py
+++ mhfp_tmap.py
@@ -1,3 +1,7 @@
+import warnings
+from tqdm import tqdm
+import tmap as tm
+import pandas as pd
 import os
 import pickle
 import warnings
@@ -10,89 +14,49 @@ from mhfp.encoder import MHFPEncoder
 from rdkit import Chem
 from tqdm import tqdm
 
-# Suppress specific warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
 
-def encode_fp(fingerprint, encoder):
+def calculate_fingerprints(df, encoder):
     """
-    Encode a SMILES string into a fingerprint.
-    """
-    try:
-        with warnings.catch_warnings(record=True) as w: 
-            warnings.simplefilter("always") 
-            enc_fingerprint = tm.VectorUint(encoder.encode(fingerprint))
-            if w and issubclass(w[-1].category, UserWarning):
-                print(f"Warning for Fingerprints {fingerprint}: {str(w[-1].message)}")
-                return None
-        return enc_fingerprint
-    except Exception as e:
-        print(f"Error encoding Fingerprints {fingerprint}: {str(e)}")
-        return None
-
-
-def calculate_fingerprints2(df, encoder, fingerprints_file):
-    """
-    Calculate fingerprints for SMILES strings, using a pickle file if available.
-    """
-    if os.path.exists(fingerprints_file):
-        print("Loading DataFrame with fingerprints from pickle file...")
-        return pd.read_pickle(fingerprints_file)
-
-    print("Calculating fingerprints...")
-    tqdm.pandas(desc="Encoding SMILES")
-    df['fingerprint'] = df['canonical_smiles'].progress_apply(lambda x: encode_smiles(x, encoder))
+    Calculate fingerprints for SMILES strings in the dataframe.
     
-    # Remove rows with None fingerprints
-    df = df.dropna(subset=['fingerprint']).reset_index(drop=True)
-
-    # Save DataFrame to pickle file
-    df.to_pickle(fingerprints_file)
-
-    return df
-
-
-def calculate_fingerprints(df, encoder, fingerprints_file):
+    Args:
+    df (pandas.DataFrame): DataFrame containing 'canonical_smiles' column
+    encoder: The encoder object used to encode SMILES strings (e.g. MHFP)
+    
+    Returns:
+    tuple: (list of encoded fingerprints, list of valid indices)
     """
-    Calculate fingerprints for SMILES strings, using a pickle file if available.
-    """
-
-    # Apparently VectorUint objects are not pickable, instead I will save the new dataset with calculated fingerprints 
-    """
-    if os.path.exists(fingerprints_file):
-        print("Loading fingerprints from pickle file...")
-        with open(fingerprints_file, 'rb') as f:
-            return pickle.load(f)
-    """
-
     fingerprints = []
     valid_indices = []
+    
     for idx, smiles in enumerate(tqdm(df['canonical_smiles'], desc="Calculating fingerprints from SMILES")):
-        fingerprint = encode_fp(smiles, encoder)
-        if fingerprint is not None:
-            fingerprints.append(fingerprint)
+        try:
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                enc_fingerprint = tm.VectorUint(encoder.encode(smiles))
+                if w and issubclass(w[-1].category, UserWarning):
+                    print(f"Warning for Fingerprints {smiles}: {str(w[-1].message)}")
+                    continue
+            fingerprints.append(enc_fingerprint)
             valid_indices.append(idx)
-
-    """    
-    # Save fingerprints to pickle file
-    with open(fingerprints_file, 'wb') as f:
-        pickle.dump(fingerprints, f)
-    """
+        except Exception as e:
+            print(f"Error encoding Fingerprints {smiles}: {str(e)}")
+    
     return fingerprints, valid_indices
 
 def main():
     # File paths
     csv_file = r'C:\Users\biolab\Desktop\Alex\Chembl_data\alex_dataset.csv'
-    fingerprints_file = 'fingerprints.pkl'
 
     # Load data
     df = pd.read_csv(csv_file)
 
     # Initialize encoder
     perm = 512  # Number of permutations used by the MinHashing algorithm
-    enc = MHFPEncoder(perm)
+    mhfp_enc = MHFPEncoder(perm)
 
     # Calculate or load fingerprints
-    fingerprints, valid_indices = calculate_fingerprints(df, enc, fingerprints_file)
+    fingerprints, valid_indices = calculate_fingerprints(df, mhfp_enc)
 
     # Update DataFrame with valid entries only
     df = df.iloc[valid_indices].reset_index(drop=True)
@@ -112,7 +76,6 @@ def main():
     cfg.sl_scaling_type = tm.RelativeToAvgLength
     x, y, s, t, _ = tm.layout_from_lsh_forest(lf, cfg)
 
-    print("\n")
     print("Plotting...")
     # Plotting
     f = Faerun(
@@ -146,5 +109,4 @@ if __name__ == "__main__":
     main()
     end_time = timer()
     print('TMAP successfully generated.')
-    print('\n')
     print(f"Total execution time: {(end_time - start_time)/60:.2f} minutes")
